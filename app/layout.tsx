d8f1dd48b3f4dd0f14d9a47d6bca277bd2a5b212
isai-santiago <isai@institutoinadaptados.edu.mx>
2026-04-01 16:18:23 -0500

    Type children prop in RootLayout as React.ReactNode
    Add type annotation for children prop in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import './globals.css';
 import CasinoLayout from '@/components/CasinoLayout';
+
 export const metadata = {
   title: 'Teto!live',
   description: 'The best place to coinflip and open cases.',
 };
 
-export default function RootLayout({ children }) {
+// ¡Aquí está la magia! Le decimos que children es de tipo React.ReactNode
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body className="bg-[#0b0e14] m-0 p-0">
